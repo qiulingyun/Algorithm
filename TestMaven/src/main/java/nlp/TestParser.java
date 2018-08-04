9c1ede0da2be759932b74782a2fec83045dbecce
i322460 <ccdice@DESKTOP-KTK9CAR>
2018-08-04 21:27:58 +0800

    word parse update 2

--- TestMaven/src/main/java/nlp/TestParser.java
+++ TestMaven/src/main/java/nlp/TestParser.java
@@ -2,17 +2,16 @@ package nlp;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.apache.ibatis.ognl.Token;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -25,6 +24,7 @@ public class TestParser {
 	public HashMap<Integer, String> lineNum2OriTxtMap = new HashMap<Integer, String>();
 	public HashMap<Integer, ArrayList<String>> parsedMap = new HashMap<Integer, ArrayList<String>>();
 	public HashMap<String, List<String>> stdwordMap = null;
+	public HashMap<String, Object> resultMap = new HashMap<String, Object>();
 	
 	public void getOriginText(String txtUrl) throws IOException{
 		URL url = TestParser.class.getClassLoader().getResource(txtUrl);
@@ -76,6 +76,46 @@ public class TestParser {
 		stdwordMap = ep.getStdWordMap();
 	}
 	
+	public void getResult(){
+		if(lineNum2OriTxtMap.isEmpty()){
+			return;
+		}
+		if(stdwordMap.isEmpty()){
+			return;
+		}
+		for(String s: lineNum2OriTxtMap.values()){
+			for(Map.Entry<String, List<String>> stdWord : stdwordMap.entrySet()){
+				String currentWord = stdWord.getKey().toLowerCase();
+				String pattern = currentWord + " (\\d+)";
+				Pattern p = Pattern.compile(pattern);
+				Matcher m = p.matcher(s.toLowerCase().trim());
+				if(m.find()){
+//					System.out.println(m.group(1));
+					resultMap.put(stdWord.getKey(), m.group(1));
+					continue;
+				}
+				
+				List<String> otherWordList = stdWord.getValue();
+				boolean findInOtherWord = false;
+				for(String otherWord : otherWordList){
+					pattern = otherWord.toLowerCase() + " (\\d+)";
+					p = Pattern.compile(pattern);
+					m = p.matcher(s.toLowerCase().trim());
+					if(m.find()){
+						resultMap.put(stdWord.getKey(), m.group(1));
+						findInOtherWord = true;
+						break;
+					}
+				}
+				if(findInOtherWord){
+					continue;
+				}
+				
+			}
+		}
+		System.out.println(resultMap);
+	}
+	
 	public static void main(String[] args) throws IOException {
 		TestParser tp = new TestParser();
 		tp.getOriginText("mail.txt");
@@ -83,7 +123,7 @@ public class TestParser {
 		
 		tp.loadStdWordMap("stdword.xlsx");
 		
-        
+        tp.getResult();
 
 	}
 
